7280e8706a9ff6e5b0cf1fa62f1432444d470334
Amilkar Dominguez <amilkar.dominguez@gmail.com>
2026-02-24 23:04:20 -0400

    chore: cards and list components

---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss',
+})
+export class ListComponent {}
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
+import { ListComponent } from '../../components/list/list.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [SearchBarComponent],
+  imports: [SearchBarComponent, ListComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
